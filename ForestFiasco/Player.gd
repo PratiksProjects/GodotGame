333b5088d7d158d71937d6ee8178cf20baa43c00
Pratik <pratiksharma1@gmail.com>
2018-04-12 22:54:10 -0400

    Added background, added floor and following camera

--- ForestFiasco/Player.gd
+++ ForestFiasco/Player.gd
@@ -5,7 +5,7 @@ var screensize
 
 func _ready():
     screensize = get_viewport_rect().size
-
+    
 func _process(delta):
     var velocity = Vector2() # the player's movement vector
     if Input.is_action_pressed("ui_right"):
@@ -22,8 +22,8 @@ func _process(delta):
     else:
         $AnimatedSprite.stop()
     position += velocity * delta
-    position.x = clamp(position.x, 0, screensize.x)
-    position.y = clamp(position.y, 0, screensize.y)
+    #position.x = clamp(position.x, 0, screensize.x)
+    position.y = clamp(position.y, 0, 250)
     if velocity.x != 0:
         $AnimatedSprite.animation = "right"
         $AnimatedSprite.flip_v = false
