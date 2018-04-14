a781c782bb0d20c72dac7841001af325c94ec625
BrandonWeiner <bweiner6@gatech.edu>
2018-04-14 14:23:14 -0400

    Added Jump, Increase Player Walk Speed
    Jump functionality was added.
    The down button no longer does anything.
    The player walks at a faster speed.

--- ForestFiasco/Player.gd
+++ ForestFiasco/Player.gd
@@ -2,37 +2,55 @@ extends Area2D
 signal hit
 export (int) var SPEED
 var screensize
+const GRAVITY = 500
+var velocity = Vector2() # the player's movement vector
+var Jumped=0
 
 func _ready():
     screensize = get_viewport_rect().size
     
+    
 func _process(delta):
-    var velocity = Vector2() # the player's movement vector
+    print(velocity.y)
+    velocity.y += delta * GRAVITY
     if Input.is_action_pressed("ui_right"):
-        velocity.x += 1
+        velocity.x += 100
     if Input.is_action_pressed("ui_left"):
-        velocity.x -= 1
+        velocity.x -= 100
     if Input.is_action_pressed("ui_down"):
-        velocity.y += 1
-    if Input.is_action_pressed("ui_up"):
-        velocity.y -= 1
+        pass
+    if Input.is_action_pressed("ui_up"): 
+        if Jumped==0:
+            velocity.y -= delta*GRAVITY*220
+            Jumped=1
+    
+    if velocity.y > 90 && velocity.y<110 && position.y>=248 && position.y<=252:
+        Jumped=0
+
     if velocity.length() > 0:
-        velocity = velocity.normalized() * SPEED
-        $AnimatedSprite.play()
+#        velocity = velocity.normalized() * SPEED
+       $AnimatedSprite.play()
     else:
         $AnimatedSprite.stop()
-    position += velocity * delta
+    position.x += velocity.x * delta
+    position.y += velocity.y * delta
     #position.x = clamp(position.x, 0, screensize.x)
     position.x = clamp(position.x, 0, 2000)
     position.y = clamp(position.y, 0, 250)
-	
-    if velocity.x != 0:
+    velocity.x = clamp(velocity.x, -300, 300)
+    velocity.y = clamp(velocity.y, -100, 100)
+    
+    if velocity.x>=0 && velocity.x<5 && velocity.y<=100 && velocity.y>95:
+         $AnimatedSprite.animation = "default"
+    elif velocity.y != 100:
+        $AnimatedSprite.animation = "up"
+    elif velocity.x != 0:
         $AnimatedSprite.animation = "right"
         $AnimatedSprite.flip_v = false
         $AnimatedSprite.flip_h = velocity.x < 0
-    elif velocity.y != 0:
-        $AnimatedSprite.animation = "up"
-        $AnimatedSprite.flip_v = velocity.y > 0
+   
+#        $AnimatedSprite.flip_v = velocity.y > 0
+    velocity.x = 0
 
 func _on_Player_body_entered(body):
     hide()
@@ -43,3 +61,5 @@ func start(pos):
     position = pos
     show()
     $CollisionShape2D.disabled = false
+    
+    
