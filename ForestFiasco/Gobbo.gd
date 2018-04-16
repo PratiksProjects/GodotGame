9fdb0e72a5890840b9a03acde911382ef23e3c39
BrandonWeiner <bweiner6@gatech.edu>
2018-04-15 22:18:34 -0400

    hpmp bars, damage mechanic
    Hp and Mp bars were added and if player hits gobbo, hp bar decreases

--- ForestFiasco/Gobbo.gd
+++ ForestFiasco/Gobbo.gd
@@ -2,23 +2,25 @@ extends RigidBody2D
 var i = 0
 export (int) var speed
 var direction = Vector2(2,0)
+var init
 func _ready():
     $AnimatedSprite.animation = "right"
-
+    init = position
 
 func _process(delta):
-    position +=direction
-    if(position.x >= 500 and i == 0):
-        i = 1
-        direction=-1*direction
-        $AnimatedSprite.flip_h = !$AnimatedSprite.flip_h
-    else:
-        if(position.x <= 100 and i == 1):
-            i = 0
+    if (position.x!=null):
+        position +=direction
+        if(position.x >= init.x+500 and i == 0):
+            i = 1
             direction=-1*direction
             $AnimatedSprite.flip_h = !$AnimatedSprite.flip_h
         else:
-            position += direction
+            if(position.x <= init.x-100 and i == 1):
+                i = 0
+                direction=-1*direction
+                $AnimatedSprite.flip_h = !$AnimatedSprite.flip_h
+            else:
+                position += direction
 
 func _on_Visibility_screen_exited():
     queue_free()
--- ForestFiasco/Player.gd
+++ ForestFiasco/Player.gd
@@ -7,67 +7,74 @@ var velocity = Vector2() # the player's movement vector
 var Jumped=0
 var onFloor=1
 var stat = Hpmp.new()
+var dead=0
+var hp=100
 
 func _ready():
     screensize = get_viewport_rect().size
     
     
 func _process(delta):
-
+    if(dead==0):
 #    velocity.y += delta * GRAVITY
-    var new = Phys.new()
-    var y=new.grav(velocity.y,delta,GRAVITY)
-    velocity.y=y
-    if Input.is_action_pressed("ui_right"):
-        velocity.x += 100
-    if Input.is_action_pressed("ui_left"):
-        velocity.x -= 100
-    if Input.is_action_pressed("ui_down"):
-        pass
-    if Input.is_action_pressed("ui_up"): 
-        if Jumped==0:
-            velocity.y -= delta*GRAVITY*800
-            Jumped=1
+        var new = Phys.new()
+        var y=new.grav(velocity.y,delta,GRAVITY)
+        velocity.y=y
+        if Input.is_action_pressed("ui_right"):
+            velocity.x += 100
+        if Input.is_action_pressed("ui_left"):
+            velocity.x -= 100
+        if Input.is_action_pressed("ui_down"):
+            pass
+        if Input.is_action_pressed("ui_up"): 
+            if Jumped==0:
+                velocity.y -= delta*GRAVITY*800
+                Jumped=1
+        
+        if velocity.y > 90 && velocity.y<110:
+            Jumped=0
     
-    if velocity.y > 90 && velocity.y<110:
-        Jumped=0
-
-    if velocity.length() > 0:
-#        velocity = velocity.normalized() * SPEED
-       $AnimatedSprite.play()
-    else:
-        $AnimatedSprite.stop()
-    position.x += velocity.x * delta
-    position.y += velocity.y * delta
-    #position.x = clamp(position.x, 0, screensize.x)
-    position.x = clamp(position.x, 0, 2000)
-    position.y = clamp(position.y, 0, 250)
-    velocity.x = clamp(velocity.x, -300, 300)
-    velocity.y = clamp(velocity.y, -100, 100)
-    
-    if velocity.x>=0 && velocity.x<5 && velocity.y<=100 && velocity.y>95:
-         $AnimatedSprite.animation = "default"
-    elif velocity.y != 100:
-        $AnimatedSprite.animation = "up"
-    elif velocity.x != 0:
-        $AnimatedSprite.animation = "right"
-        $AnimatedSprite.flip_v = false
-        $AnimatedSprite.flip_h = velocity.x < 0
-   
-#        $AnimatedSprite.flip_v = velocity.y > 0
-    velocity.x = 0
+        if velocity.length() > 0:
+    #        velocity = velocity.normalized() * SPEED
+           $AnimatedSprite.play()
+        else:
+            $AnimatedSprite.stop()
+        position.x += velocity.x * delta
+        position.y += velocity.y * delta
+        #position.x = clamp(position.x, 0, screensize.x)
+        position.x = clamp(position.x, 0, 2000)
+        position.y = clamp(position.y, 0, 250)
+        velocity.x = clamp(velocity.x, -300, 300)
+        velocity.y = clamp(velocity.y, -100, 100)
+        
+        if velocity.x>=0 && velocity.x<5 && velocity.y<=100 && velocity.y>95:
+             $AnimatedSprite.animation = "default"
+        elif velocity.y != 100:
+            $AnimatedSprite.animation = "up"
+        elif velocity.x != 0:
+            $AnimatedSprite.animation = "right"
+            $AnimatedSprite.flip_v = false
+            $AnimatedSprite.flip_h = velocity.x < 0
+       
+    #        $AnimatedSprite.flip_v = velocity.y > 0
+        velocity.x = 0
 
 func _on_Player_body_entered(body):
+  
     if(body.get_name() == "gobboChild"):
 #        hide()
-        var y = stat.stats(0,-1)
-        print(y)
+        hp = stat.stats(0,-99)
+        print(hp)
+        $Camera2D/HPBar.set_value(hp)
     elif(body.get_name() == "platformChild"):
         velocity.y -= GRAVITY
     else:
         print("not gobbo or platform")
     #$CollisionShape2D.disabled = true
-    
+    if(hp<=0):
+        hide()
+        $CollisionShape2D.disabled = true
+        dead=1
 #func overlaps_body(body):
 #    if(body.get_name() == "platformChild"):
 #        velocity.y -= GRAVITY
